e9e5f262a4b817047635cfd0f9006695a6d2e91f
7-s3m-7 <94192922+7-s3m-7@users.noreply.github.com>
2022-05-26 14:37:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 sudo cp -r ~/one-dark-i3/Wallpapers ~/Wallpapers
 sudo cp ~/one-dark-i3/i3/config ~/.config/i3/config
 sudo mkdir ~/.config/i3status
-sudo touch ~/.config/i3status/config
 sudo cp ~/one-dark-i3/i3status/config ~/.config/i3status/config
 sudo mkdir ~/.config/alacritty
-sudo touch ~/.config/alacritty/alacritty.yml
 sudo cp ~/one-dark-i3/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+sudo cp ~/one-dark-i3/picom.conf ~/.config/picom.conf
+sudo mkdir ~/.vim
+sudo mkdir ~/.vim/colors
+sudo mkdir ~/.vim/autoload
+sudo cp ~/one-dark-i3/.vim/colors/onedark.vim ~/.vim/colors/onedark.vim
+sudo cp ~/one-dark-i3/.vim/autoload/onedark.vim ~/.vim/autoload/onedark.vim
+sudo cp ~/one-dark-i3/.vimrc ~/
